f3d6bf56ed8856f1ece7499391b2467221ac1b0a
Lorenzo Leite <lorenzokleite@gmail.com>
2021-06-26 02:29:22 -0300

    Finished application

--- src/route.js
+++ src/route.js
@@ -5,11 +5,15 @@ const route = express.Router();
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}));
-route.get('/room/:room', (req, res) => res.render("room"));
+
+route.post('/create-room', RoomController.create);
+route.get('/room/:room', RoomController.open);
+route.post('/enter-room', RoomController.enter); //post: enviando formulário
 
 //Formato que o formulario de dentro da modal tem que passar informação
+route.post('/question/create/:room', QuestionController.create);
 route.post('/question/:room/:question/:action', QuestionController.index);
-route.post('/create-room', RoomController.create);
 
 
-module.exports = route; 
+
+module.exports = route;
